29f0aec0b2e86d9ca531a6b5a72d936bc15f5ad6
lipeng888 <1866410301@163.com>
2018-09-09 14:19:03 +0800

    第一次提交

--- src/cn/itcast/a_hello/DemoServlet.java
+++ src/cn/itcast/a_hello/DemoServlet.java
@@ -15,6 +15,7 @@ public class DemoServlet extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	System.out.println("-----------doGet---------");
 	}
 
 }
